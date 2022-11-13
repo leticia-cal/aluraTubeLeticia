169e2eee0fa13ecf9794ed89fc8269b43441f797
Le Calixter <lehcalixter@gmail.com>
2022-11-13 16:21:14 -0300

    atualização aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estilosDaHomePage = {
-    // backgroundColor: "red" 
-  };
 
-  // console.log(config.playlists);
   const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
   return (
     <>
-      <CSSReset />
       <div style={{
         display: "flex",
         flexDirection: "column",
@@ -36,6 +30,7 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
+    background-color: ${({theme}) => theme.backgroundLevel1};
     .user-info > img{
       width: 80px;
       height: 80px;
